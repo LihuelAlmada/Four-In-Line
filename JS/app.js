08c450b7d3ead6ff292ac0c2f0ebfc706cdb6d6d
Lihuel Almada <Lihuel.Almada@gmail.com>
2020-06-05 18:17:54 -0300

    logic create

--- JS/app.js
+++ JS/app.js
@@ -1,52 +1,132 @@
-var turn = true
+var turn = true;
+var charge1 = 0;
+var charge2 = 0;
+var charge3 = 0;
+var charge4 = 0;
+var charge5 = 0;
+var charge6 = 0;
+var charge7 = 0;
+var colorRow = '#FFCC00';
 
 function turnChange() {
     if (turn === true) {
         turn = false
+        colorRow = '#FF6447';
         console.log('turn You');
     } else {
         turn = true
+        colorRow = '#FFCC00';
         console.log('turn AI');
     }
 }
 
 function c1() {
-    console.log('C1');
-    turnChange();
+    if (charge1 <= 6) {
+        console.log('C1');
+        if (turn === true) {
+            //Pintar de amarillo
+
+        } else {
+            //Pintar de rojo
+        }
+
+        if (charge1 = 1) {
+            r1c1.style.background = colorRow;
+        }
+        charge1++
+        console.log(charge1);
+        turnChange();
+    }
+
+
 }
 
 function c2() {
-    console.log('C2');
-    turnChange();
+    if (charge2 <= 6) {
+        console.log('C2');
+        if (turn === true) {
+            //Pintar de amarillo
+        } else {
+            //Pintar de rojo
+        }
+        charge2++
+        console.log(charge2);
+        turnChange();
+    }
 }
 
 function c3() {
-    console.log('C3');
-    turnChange();
+    if (charge3 <= 6) {
+        console.log('C3');
+        if (turn === true) {
+            //Pintar de amarillo
+        } else {
+            //Pintar de rojo
+        }
+        charge3++
+        console.log(charge3);
+        turnChange();
+    }
 }
 
 function c4() {
-    console.log('C4');
-    turnChange();
+    if (charge4 <= 6) {
+        console.log('C4');
+        if (turn === true) {
+            //Pintar de amarillo
+        } else {
+            //Pintar de rojo
+        }
+        charge4++
+        console.log(charge4);
+        turnChange();
+    }
 }
 
 function c5() {
-    console.log('C5');
-    turnChange();
+    if (charge5 <= 6) {
+        console.log('C5');
+        if (turn === true) {
+            //Pintar de amarillo
+        } else {
+            //Pintar de rojo
+        }
+        charge5++
+        console.log(charge5);
+        turnChange();
+    }
 }
 
 function c6() {
-    console.log('C6');
-    turnChange();
+    if (charge6 <= 6) {
+        console.log('C6');
+        if (turn === true) {
+            //Pintar de amarillo
+        } else {
+            //Pintar de rojo
+        }
+        charge6++
+        console.log(charge6);
+        turnChange();
+    }
 }
 
 function c7() {
-    console.log('C7');
-    turnChange();
+    if (charge7 <= 6) {
+        console.log('C7');
+        if (turn === true) {
+            //Pintar de amarillo
+        } else {
+            //Pintar de rojo
+        }
+        charge7++
+        console.log(charge7);
+        turnChange();
+    }
 }
 
 window.onload = function() {
-
+    r1c1 = document.getElementById('row1c1');
     column1 = document.getElementById('column1');
     column2 = document.getElementById('column2');
     column3 = document.getElementById('column3');
