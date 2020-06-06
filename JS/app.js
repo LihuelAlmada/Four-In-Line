ff6b5bd414fa64578d33e425f59fee961fd0376a
Lihuel Almada <Lihuel.Almada@gmail.com>
2020-06-05 21:39:33 -0300

    All colors

--- JS/app.js
+++ JS/app.js
@@ -21,112 +21,313 @@ function turnChange() {
 }
 
 function c1() {
-    if (charge1 <= 6) {
-        console.log('C1');
-        if (turn === true) {
-            //Pintar de amarillo
+    switch (charge1) {
+        case 0:
+            r6c1.style.background = colorRow;
+            break;
 
-        } else {
-            //Pintar de rojo
-        }
+        case 1:
+            r5c1.style.background = colorRow;
+            break;
 
-        if (charge1 = 1) {
+        case 2:
+            r4c1.style.background = colorRow;
+            break;
+
+        case 3:
+            r3c1.style.background = colorRow;
+            break;
+
+        case 4:
+            r2c1.style.background = colorRow;
+            break;
+
+        case 5:
             r1c1.style.background = colorRow;
-        }
-        charge1++
-        console.log(charge1);
-        turnChange();
+            break;
+
+        default:
+            console.log('No hay mas espacio');
+            break;
+
     }
-
-
+    charge1++
+    console.log(charge1);
+    turnChange();
 }
 
 function c2() {
-    if (charge2 <= 6) {
-        console.log('C2');
-        if (turn === true) {
-            //Pintar de amarillo
-        } else {
-            //Pintar de rojo
-        }
-        charge2++
-        console.log(charge2);
-        turnChange();
+    switch (charge2) {
+        case 0:
+            r6c2.style.background = colorRow;
+            break;
+
+        case 1:
+            r5c2.style.background = colorRow;
+            break;
+
+        case 2:
+            r4c2.style.background = colorRow;
+            break;
+
+        case 3:
+            r3c2.style.background = colorRow;
+            break;
+
+        case 4:
+            r2c2.style.background = colorRow;
+            break;
+
+        case 5:
+            r1c2.style.background = colorRow;
+            break;
+
+        default:
+            console.log('No hay mas espacio');
+            break;
+
     }
+    charge2++
+    console.log(charge2);
+    turnChange();
+
 }
 
 function c3() {
-    if (charge3 <= 6) {
-        console.log('C3');
-        if (turn === true) {
-            //Pintar de amarillo
-        } else {
-            //Pintar de rojo
-        }
-        charge3++
-        console.log(charge3);
-        turnChange();
+    switch (charge3) {
+        case 0:
+            r6c3.style.background = colorRow;
+            break;
+
+        case 1:
+            r5c3.style.background = colorRow;
+            break;
+
+        case 2:
+            r4c3.style.background = colorRow;
+            break;
+
+        case 3:
+            r3c3.style.background = colorRow;
+            break;
+
+        case 4:
+            r2c3.style.background = colorRow;
+            break;
+
+        case 5:
+            r1c3.style.background = colorRow;
+            break;
+
+        default:
+            console.log('No hay mas espacio');
+            break;
+
     }
+    charge3++
+    console.log(charge3);
+    turnChange();
+
 }
 
 function c4() {
-    if (charge4 <= 6) {
-        console.log('C4');
-        if (turn === true) {
-            //Pintar de amarillo
-        } else {
-            //Pintar de rojo
-        }
-        charge4++
-        console.log(charge4);
-        turnChange();
+    switch (charge4) {
+        case 0:
+            r6c4.style.background = colorRow;
+            break;
+
+        case 1:
+            r5c4.style.background = colorRow;
+            break;
+
+        case 2:
+            r4c4.style.background = colorRow;
+            break;
+
+        case 3:
+            r3c4.style.background = colorRow;
+            break;
+
+        case 4:
+            r2c4.style.background = colorRow;
+            break;
+
+        case 5:
+            r1c4.style.background = colorRow;
+            break;
+
+        default:
+            console.log('No hay mas espacio');
+            break;
+
     }
+    charge4++
+    console.log(charge4);
+    turnChange();
+
 }
 
 function c5() {
-    if (charge5 <= 6) {
-        console.log('C5');
-        if (turn === true) {
-            //Pintar de amarillo
-        } else {
-            //Pintar de rojo
-        }
-        charge5++
-        console.log(charge5);
-        turnChange();
+    switch (charge5) {
+        case 0:
+            r6c5.style.background = colorRow;
+            break;
+
+        case 1:
+            r5c5.style.background = colorRow;
+            break;
+
+        case 2:
+            r4c5.style.background = colorRow;
+            break;
+
+        case 3:
+            r3c5.style.background = colorRow;
+            break;
+
+        case 4:
+            r2c5.style.background = colorRow;
+            break;
+
+        case 5:
+            r1c5.style.background = colorRow;
+            break;
+
+        default:
+            console.log('No hay mas espacio');
+            break;
+
     }
+    charge5++
+    console.log(charge5);
+    turnChange();
+
 }
 
 function c6() {
-    if (charge6 <= 6) {
-        console.log('C6');
-        if (turn === true) {
-            //Pintar de amarillo
-        } else {
-            //Pintar de rojo
-        }
-        charge6++
-        console.log(charge6);
-        turnChange();
+    switch (charge6) {
+        case 0:
+            r6c6.style.background = colorRow;
+            break;
+
+        case 1:
+            r5c6.style.background = colorRow;
+            break;
+
+        case 2:
+            r4c6.style.background = colorRow;
+            break;
+
+        case 3:
+            r3c6.style.background = colorRow;
+            break;
+
+        case 4:
+            r2c6.style.background = colorRow;
+            break;
+
+        case 5:
+            r1c6.style.background = colorRow;
+            break;
+
+        default:
+            console.log('No hay mas espacio');
+            break;
+
     }
+    charge6++
+    console.log(charge6);
+    turnChange();
+
 }
 
 function c7() {
-    if (charge7 <= 6) {
-        console.log('C7');
-        if (turn === true) {
-            //Pintar de amarillo
-        } else {
-            //Pintar de rojo
-        }
-        charge7++
-        console.log(charge7);
-        turnChange();
+    switch (charge7) {
+        case 0:
+            r6c7.style.background = colorRow;
+            break;
+
+        case 1:
+            r5c7.style.background = colorRow;
+            break;
+
+        case 2:
+            r4c7.style.background = colorRow;
+            break;
+
+        case 3:
+            r3c7.style.background = colorRow;
+            break;
+
+        case 4:
+            r2c7.style.background = colorRow;
+            break;
+
+        case 5:
+            r1c7.style.background = colorRow;
+            break;
+
+        default:
+            console.log('No hay mas espacio');
+            break;
+
     }
+    charge7++
+    console.log(charge7);
+    turnChange();
+
 }
 
 window.onload = function() {
     r1c1 = document.getElementById('row1c1');
+    r2c1 = document.getElementById('row2c1');
+    r3c1 = document.getElementById('row3c1');
+    r4c1 = document.getElementById('row4c1');
+    r5c1 = document.getElementById('row5c1');
+    r6c1 = document.getElementById('row6c1');
+
+    r1c2 = document.getElementById('row1c2');
+    r2c2 = document.getElementById('row2c2');
+    r3c2 = document.getElementById('row3c2');
+    r4c2 = document.getElementById('row4c2');
+    r5c2 = document.getElementById('row5c2');
+    r6c2 = document.getElementById('row6c2');
+
+    r1c3 = document.getElementById('row1c3');
+    r2c3 = document.getElementById('row2c3');
+    r3c3 = document.getElementById('row3c3');
+    r4c3 = document.getElementById('row4c3');
+    r5c3 = document.getElementById('row5c3');
+    r6c3 = document.getElementById('row6c3');
+
+    r1c4 = document.getElementById('row1c4');
+    r2c4 = document.getElementById('row2c4');
+    r3c4 = document.getElementById('row3c4');
+    r4c4 = document.getElementById('row4c4');
+    r5c4 = document.getElementById('row5c4');
+    r6c4 = document.getElementById('row6c4');
+
+    r1c5 = document.getElementById('row1c5');
+    r2c5 = document.getElementById('row2c5');
+    r3c5 = document.getElementById('row3c5');
+    r4c5 = document.getElementById('row4c5');
+    r5c5 = document.getElementById('row5c5');
+    r6c5 = document.getElementById('row6c5');
+
+    r1c6 = document.getElementById('row1c6');
+    r2c6 = document.getElementById('row2c6');
+    r3c6 = document.getElementById('row3c6');
+    r4c6 = document.getElementById('row4c6');
+    r5c6 = document.getElementById('row5c6');
+    r6c6 = document.getElementById('row6c6');
+
+    r1c7 = document.getElementById('row1c7');
+    r2c7 = document.getElementById('row2c7');
+    r3c7 = document.getElementById('row3c7');
+    r4c7 = document.getElementById('row4c7');
+    r5c7 = document.getElementById('row5c7');
+    r6c7 = document.getElementById('row6c7');
+
     column1 = document.getElementById('column1');
     column2 = document.getElementById('column2');
     column3 = document.getElementById('column3');
