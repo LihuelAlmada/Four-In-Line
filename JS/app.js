a11e3bc9880fff929bfa0d6735718af7553ca8d0
Necralma <lihuel.almada@gmail.com>
2020-06-17 18:47:47 -0300

    semicolon

--- JS/app.js
+++ JS/app.js
@@ -1,17 +1,15 @@
-var boardHTML = null
-var columnsHTML = null
-
-var turn = 'yellow'
-var board = [
-  [null, null, null, null, null, null],
-  [null, null, null, null, null, null],
-  [null, null, null, null, null, null],
-  [null, null, null, null, null, null],
-  [null, null, null, null, null, null],
-  [null, null, null, null, null, null],
-  [null, null, null, null, null, null]
-]
-
+var boardHTML = null,
+    columnsHTML = null,
+    turn = 'yellow',
+    board = [
+    [null, null, null, null, null, null],
+    [null, null, null, null, null, null],
+    [null, null, null, null, null, null],
+    [null, null, null, null, null, null],
+    [null, null, null, null, null, null],
+    [null, null, null, null, null, null],
+    [null, null, null, null, null, null]
+    ];
 var toggleTurn = function () {
   console.log("entra");
   if(turn === 'yellow'){
@@ -22,46 +20,43 @@ var toggleTurn = function () {
     go.style.background = '#F5CA45';
   }
 }
-
 var columnEventHandler = function (evt) {
-  var columnId = evt.target.id.substr(1, 1)
+  var columnId = evt.target.id.substr(1, 1);
   for (var i = 0; i < board[columnId].length; i++) {
     if (!board[columnId][i]) {
-      board[columnId][i] = turn
-      toggleTurn()
-      render()
-      break
+      board[columnId][i] = turn;
+      toggleTurn();
+      render();
+      break;
     }
   }
 }
-
 var bindColumnHandlers = function () {
-  columnsHTML = document.getElementsByClassName('column')
+  columnsHTML = document.getElementsByClassName('column');
   for (var i = 0; i < columnsHTML.length; i++) {
-    columnsHTML[i].onclick = columnEventHandler
+    columnsHTML[i].onclick = columnEventHandler;
   }
 }
-
 var render = function () {
-  var html = ''
+  var html = '';
   for (var i = 0; i < board.length; i++) {
-    html += '<div id="c' + i + '" class="column">'
+    html += '<div id="c' + i + '" class="column">';
     for (var j = board[i].length - 1; j >= 0; j--) {
-      html += '<div id="s' + i + j + '" class="spot'
+      html += '<div id="s' + i + j + '" class="spot';
       if (board[i][j]) html += ' ' + board[i][j]
-      html += '"></div>'
+      {
+        html += '"></div>';
+      }
     }
-    html += '</div>'
+    html += '</div>';
   }
-  boardHTML.innerHTML = html
-  bindColumnHandlers()
+  boardHTML.innerHTML = html;
+  bindColumnHandlers();
 }
-
 var init = function () {
   boardHTML = document.getElementById('board');
   go = document.getElementById('go');
-  turn = Math.random() > 0.5 ? 'yellow' : 'red'
-  render()
+  turn = Math.random() > 0.5 ? 'yellow' : 'red';
+  render();
 }
-
-window.onload = init
+window.onload = init;
